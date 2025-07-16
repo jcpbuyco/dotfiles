7671fe49f596d6dae06c0e835814111c8ff1c511
JC Buyco <jcpbuyco@gmail.com>
2025-07-16 02:52:57 +0200

    minor changes to nvim config

--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -10,6 +10,14 @@ return {
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    'rcarriga/nvim-notify',
+    {
+      'rcarriga/nvim-notify',
+      opts = {
+        background_colour = '#000000',
+        render = 'wrapped-compact',
+        fps = 120,
+        stages = 'fade',
+      },
+    },
   },
 }
